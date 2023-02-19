6facecfd36f6bcc7c383793844f78aa1504db3b1
AlexeyAleksandrov <chinok73@yandex.ru>
2023-02-19 23:43:09 +0300

    Доработаны комментарии

--- src/main/java/ru/phasemicroscope/Application.java
+++ src/main/java/ru/phasemicroscope/Application.java
@@ -6,11 +6,9 @@ import ru.phasemicroscope.window.Render;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.Raster;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.function.Consumer;
 
 public class Application
 {
@@ -24,32 +22,33 @@ public class Application
 
         image = ImageIO.read(new File("src/main/resources/lines2.jpg"));
 
-        int width = image.getWidth();
-        int height = image.getHeight();
+        int width = image.getWidth();       // ширина изображения
+        int height = image.getHeight();     // высота изображения
 
-        double[] avgGrayValues = new double[width];
+        double[] avgGrayValues = new double[width];     // массив среднего арифметического значения в градациях серого по каждой вертикале
 
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < width; x++)     // проходим по каждому столбцу
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < height; y++)    // проходим по каждому значению в столбце
             {
-                int rgb = image.getRGB(x, y);
+                int rgb = image.getRGB(x, y);   // получаем RGB значения пикселя
+
                 int r = (rgb >> 16) & 0xFF;
                 int g = (rgb >> 8) & 0xFF;
                 int b = (rgb & 0xFF);
 
-                // Normalize and gamma correct:
+                // нормализация и гамма-коррекция:
                 float rr = (float) Math.pow(r / 255.0, 2.2);
                 float gg = (float) Math.pow(g / 255.0, 2.2);
                 float bb = (float) Math.pow(b / 255.0, 2.2);
 
-                // Calculate luminance:
+                // рассчитываем яркость:
                 float lum = (float) (0.2126 * rr + 0.7152 * gg + 0.0722 * bb);
 
-                // Gamma compand and rescale to byte range:
+                // гамма-сложение и масштабирование до диапазона байтов:
                 int grayLevel = (int) (255.0 * Math.pow(lum, 1.0 / 2.2));
                 int gray = (grayLevel << 16) + (grayLevel << 8) + grayLevel;
-                image.setRGB(x, y, gray);
+                image.setRGB(x, y, gray);   // перерисовываем пиксель в градации серого
 
                 avgGrayValues[x] += grayLevel;   // суммируем значения для этой позиции х
             }
@@ -57,6 +56,7 @@ public class Application
             avgGrayValues[x] /= height;  // считаем среднее арифметическое по этой вертикали
         }
 
+        // ищем границы между минимумами и максимумами
         double min = Arrays.stream(avgGrayValues).min().orElse(0);    // считаем минимальное значение
         double max = Arrays.stream(avgGrayValues).max().orElse(255);  // ищем максимальное значение
 
@@ -65,7 +65,7 @@ public class Application
         // считаем кол-во переходов графика через среднее значение - ищем кол-во минимумов/максимумов
         ArrayList<Integer> changePoints = new ArrayList<>();    // список Х точек, в которых происходит переход между минимумами и максимумами
 
-        for (int i = 0; i < avgGrayValues.length-1; i++)
+        for (int i = 0; i < avgGrayValues.length-1; i++)    // проходим по каждому среднему значению
         {
             if((avgGrayValues[i] > minMaxAvg && avgGrayValues[i+1] <= minMaxAvg)
                     || (avgGrayValues[i] < minMaxAvg && avgGrayValues[i+1] > minMaxAvg))    // если одно значение больше среднего, а другое меньше, значит мы на границе
@@ -85,15 +85,15 @@ public class Application
             double maxGray = Arrays.stream(subarray).max().orElse(minMaxAvg);   // находим максимум
             double minGray = Arrays.stream(subarray).min().orElse(minMaxAvg);   // находим минимум
 
-            double foundValue = 0;  // найденное значение минимума или максимума
+            double foundValue;  // найденное значение минимума или максимума
 
             if (maxGray > minMaxAvg)    // если найденный максимум больше, чем среднее значение
             {
-                foundValue = maxGray;
+                foundValue = maxGray;   // сохраняем максимальное значение
             }
             else    // если найденный минимум меньше, чем среднее значение
             {
-                foundValue = minGray;
+                foundValue = minGray;   // сохраняем минимальное значение
             }
 
             // ищем позицию, на которой находится найденное значение
@@ -110,11 +110,11 @@ public class Application
             // добавляем найденную координату в список
             if (maxGray > minMaxAvg)    // если найденный максимум больше, чем среднее значение
             {
-                maximumsCoordinates.add(position);
+                maximumsCoordinates.add(position);      // добавляем позицию, на которой находится максимум
             }
             else    // если найденный минимум меньше, чем среднее значение
             {
-                minimumsCoordinates.add(position);
+                minimumsCoordinates.add(position);      // добавляем позицию, на которой находится минимум
             }
 
             x1 = x2;    // приравниваем следующее значение X к текущему, для следующей итерации
@@ -124,21 +124,21 @@ public class Application
         Graphics2D graphics = image.createGraphics();
 
         // рисуем минимумы
-        graphics.setColor(Color.red);
+        graphics.setColor(Color.red);   // цвет рисования
         for (int x : minimumsCoordinates)
         {
-            int y1 = (int) ((int) ((double) height / 2) + ((double) height * 0.05));
-            int y2 = (int) ((int) ((double) height / 2) - ((double) height * 0.05));
-            graphics.drawLine(x, y1, x, y2);
+            int y1 = (int) ((int) ((double) height / 2) + ((double) height * 0.05));    // вертикальный центр +5% вверх
+            int y2 = (int) ((int) ((double) height / 2) - ((double) height * 0.05));    // вертикальный центр -5% вниз
+            graphics.drawLine(x, y1, x, y2);    // рисуем линию
         }
 
         // рисуем максимумы
-        graphics.setColor(Color.green);
+        graphics.setColor(Color.green);     // цвет рисования
         for (int x : maximumsCoordinates)
         {
-            int y1 = (int) ((int) ((double) height / 2) + ((double) height * 0.05));
-            int y2 = (int) ((int) ((double) height / 2) - ((double) height * 0.05));
-            graphics.drawLine(x, y1, x, y2);
+            int y1 = (int) ((int) ((double) height / 2) + ((double) height * 0.05));    // вертикальный центр +5% вверх
+            int y2 = (int) ((int) ((double) height / 2) - ((double) height * 0.05));    // вертикальный центр -5% вниз
+            graphics.drawLine(x, y1, x, y2);    // рисуем линию
         }
 
         render.draw(image);  // рисуем
